f22bfb34245b94f64ec110ea2bbaa47015b1b77d
Reem99-sina <reemsina2@gmail.com>
2024-02-26 04:00:48 +0200

    store

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -15,7 +15,7 @@ const CartItem = ({data}:{data:Product}) => {
     return ( 
         <li className="flex py-6 border-b">
             <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
-                <Image fill src={data?.images[0]?.url} className="object-cover object-center"/>
+                <Image fill src={data?.images[0]?.url} className="object-cover object-center" alt=""/>
             </div>
             <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
                 <div className="absolute z-10 right-0 top-0">

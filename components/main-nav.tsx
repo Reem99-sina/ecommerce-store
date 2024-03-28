d41a2329a40deb08c3c0f8c77c713e1412674fa6
Reem99-sina <reemsina2@gmail.com>
2024-03-28 02:02:00 +0200

    ehe

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,15 +13,15 @@ const MainNav:React.FC<MainNavProps>=({
   
     const pathname=usePathname()
     const routes=data.map((route)=>({
-        href:`/category/${route.id}`,
-        label:route.name,
-        active:pathname==`/category/${route.id}`
+        href:`/category/${route?.id}`,
+        label:route?.name,
+        active:pathname==`/category/${route?.id}`
     }))
 return(
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
         {routes.map((ele)=>(
-            <Link href={ele.href}  key={ele.href} className={cn("text-sm font-medium transition-colors hover:text-black",ele?.active?"text-black":"text-neutral-500")}>
-            {ele.label}
+            <Link href={ele?.href}  key={ele?.href} className={cn("text-sm font-medium transition-colors hover:text-black",ele?.active?"text-black":"text-neutral-500")}>
+            {ele?.label}
             </Link>
         ))}
     </nav>
